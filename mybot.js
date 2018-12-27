3b9f4a33abd561fdd6e36a968d5d88f8720363e3
Kurtis1928 <kurtis1928@gmail.com>
2018-12-27 16:46:30 -0500

    first commit

---
+++ mybot.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const token = require("./token.json");
+
+//const prefix = config.prefix;
+
+// Runs when the bot is initialized
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  // Only responds to messages that start with prefix
+  // Only responds to messages that are not sent by another bot
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  // Converts command input to an array of arguments
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    message.channel.send("pong!");
+  }
+  if (command === "somefap") {
+    message.channel.send("that guy has the big gay");
+  }
+});
+
+// Bot login using token
+client.login(token.botToken);
